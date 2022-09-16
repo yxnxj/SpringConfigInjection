e2fa9fee0154d01959bdebac12b5a015bcafa5f1
YeonJun Cho <micayel1219@gmail.com>
2022-09-16 09:26:37 +0900

    변경점 커밋

--- src/main/java/com/example/demo/DataController.java
+++ src/main/java/com/example/demo/DataController.java
@@ -1,16 +1,18 @@
 package com.example.demo;
 
 import com.example.demo.model.DataDto;
+import com.example.demo.model.DataForm;
 import com.example.demo.model.DataVo;
+import org.mariadb.jdbc.plugin.codec.LocalDateTimeCodec;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RequestMapping("/data")
@@ -25,9 +27,43 @@ public class DataController {
     @GetMapping("/search")
     public String search(@RequestParam(required = true, defaultValue = "1") Long id, Model model){
         DataVo vo = dataService.search(id);
+        DataDto dataDto1 = DataDto.builder()
+                .col1("col1")
+                .uuid(UUID.randomUUID())
+                .build();
+
+        DataDto dataDto2 = DataDto.builder()
+                .col1("col2")
+                .uuid(UUID.randomUUID())
+                .build();
+
+        DataDto dataDto3 = DataDto.builder()
+                .col1("col3")
+                .uuid(UUID.randomUUID())
+                .build();
+
+        DataForm dataForm = new DataForm();
+        List<DataDto> dtoList = new ArrayList<>();
+        dtoList.add(dataDto1);
+        dtoList.add(dataDto2);
+        dtoList.add(dataDto3);
+        dataForm.setDtoList(dtoList);
+
+
+        model.addAttribute("dataForm", dataForm);
+
         model.addAttribute("vo", vo.getCol1());
 
         return "data";
+    }
+
+    @PostMapping("/mark")
+    public String mark(@RequestBody DataForm dataForm, Model model){
+        List<DataDto> dtoList = dataForm.getDtoList();
+
+        System.out.println(dtoList.size());
+
+        return "redirect::";
     }
 
 }
---
+++ src/main/java/com/example/demo/model/DataForm.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class DataForm {
+    private List<DataDto> dtoList;
+}
